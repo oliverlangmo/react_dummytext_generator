382d64ebc0f9cae962fcde88e75f06f3ae827dc2
oliverlangmo <oliver.langmo@gmail.com>
2017-01-17 17:13:39 -0600

    first commit

---
+++ src/Components/App.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import Output from './Output';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      paras: 4,
+      html: true,
+      text: ''
+    }
+  }
+
+  componentWillMount(){
+    this.getText();
+  }
+
+  getText(){
+    axios.get('http://hipsterjesus.com/api?paras='+this.state.paras+'&html='+this.state.html)
+    .then((response)=>{
+      this.setState({text:response.data.text}, function(){
+        console.log(this.state);
+      });
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
+  }
+  render(){
+    return(
+      <div>
+      <h1>DummyText Generator</h1>
+      <Output value = {this.state.text} />
+      </div>
+    );
+  }
+}
+
+export default App;
